d2917732b09646d7144ac371b5f7f38aa7c51130
shaun-moate <shaun.moate@gmail.com>
2023-07-27 17:16:15 +0100

    Nova.Micrograd: implemented micrograd with basic example

--- nova_grad/engine.py
+++ nova_grad/engine.py
@@ -1,3 +1,95 @@
-def init():
-    pass
+# A scalar property is a property that can be represented as a single value. 
+# The properties that contain multiple values (either homogeneous or heterogeneous) are non-scalar properties.
+import math
+
+class Scalar:
+    def __init__(self, value, _children=(), _op="", label=""):
+        self.data = value
+        self.grad = 0.0
+        self._backward = lambda: None
+        self.label = label
+        self._connections = set(_children)
+        self._op = _op
+
+    def __add__(self, other):
+        other = other if isinstance(other, Scalar) else Scalar(other)
+        out = Scalar(self.data + other.data, (self, other), "+")
+        def _backward():
+            self.grad += 1.0 * out.grad 
+            other.grad += 1.0 * out.grad
+        out._backward = _backward
+        return out
+        
+    def __radd__(self, other): 
+        return self + other
+
+    def __neg__(self): 
+        return self * -1
+
+    def __sub__(self, other): 
+        return self + (-other)
+
+    def __rsub__(self, other): 
+        return other + (-self)
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Scalar) else Scalar(other)
+        out = Scalar(self.data * other.data, (self, other), "*")
+        def _backward():
+            self.grad += other.data * out.grad 
+            other.grad += self.data * out.grad
+        out._backward = _backward
+        return out
+
+    def __rmul__(self, other): 
+        return self * other
+
+    def __truediv__(self, other): 
+        return self * other**-1
+
+    def __rtruediv__(self, other): 
+        return other * self**-1
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+        out = Scalar(self.data**other, (self,), f'**{other}')
+        def _backward():
+            self.grad += (other * self.data**(other-1)) * out.grad
+        out._backward = _backward
+        return out
+
+    def exp(self):
+        x = self.data
+        out = Scalar(math.exp(x), (self, ), "exp")
+
+        def _backward():
+            self.grad += math.exp(self.data) * out.grad
+        out._backward = _backward
+        
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2*x) - 1) / (math.exp(2*x) + 1)
+        out = Scalar(t, (self, ), "tanh")
+        def _backward():
+            self.grad += (1 - t**2) * out.grad 
+        out._backward = _backward
+        return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._connections:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+        self.grad = 1.0
+        for v in reversed(topo):
+            v._backward()
+
+    def __repr__(self): 
+        return f"Scalar({self.data:.3f})"
+
 
--- nova_grad/neural_network.py
+++ nova_grad/neural_network.py
@@ -1,2 +1,55 @@
-def init():
-    pass
+import random
+from nova_grad.engine import Scalar
+
+class Base:
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = 0
+
+    def parameters(self):
+        return []
+
+class Neuron(Base):
+    def __init__(self, n_inputs):
+        self.weights = [Scalar(random.uniform(-1,1)) for _ in range(n_inputs)]
+        self.bias = Scalar(random.uniform(-1,1))
+
+    def __call__(self, x):
+        act = sum((wi*xi for wi,xi in zip(self.weights, x)), self.bias)
+        return act.tanh()
+
+    def parameters(self):
+        return self.weights + [self.bias]
+
+    def __repr__(self):
+        return f"Tanh Neuron({len(self.weights)})"
+
+class Layer(Base):
+    def __init__(self, n_inputs, n_outputs):
+        self.neurons = [Neuron(n_inputs) for _ in range(n_outputs)]
+
+    def __call__(self, x):
+        out = [n(x) for n in self.neurons]
+        return out[0] if len(out) == 1 else out
+
+    def parameters(self):
+        return [p for n in self.neurons for p in n.parameters()]
+
+    def __repr__(self):
+        return f"Layer of [{', '.join(str(n) for n in self.neurons)}]"
+
+class MultiLayerPerceptron(Base):
+    def __init__(self, n_inputs, n_outputs):
+        sz = [n_inputs] + n_outputs
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(n_outputs))]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
+
+    def __repr__(self):
+        return f"MLP of [{', '.join(str(layer) for layer in self.layers)}]"
